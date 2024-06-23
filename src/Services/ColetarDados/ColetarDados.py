6ff7485201ce62e503bb22d318b12637c8facdd1
antonioThomaz1903 <antoniotor987@gmail.com>
2024-06-23 19:35:34 -0400

    chore: atualizando .gitignore

---
+++ src/Services/ColetarDados/ColetarDados.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from src.Entities.DadosClimaticos import DadosClimaticos, Base
+import datetime
+from src.app import settings
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+
+API_KEY = settings.API_KEY
+DATABASE_URI = settings.DADOS_CLIMATICOS_DATABASE_URI
+
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def coletarDados(lat, lon):
+    req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}")
+    conteudo = json.loads(req.content)
+    temp = conteudo['main']['temp']
+    humidity = conteudo['main']['humidity']
+    pressure = conteudo['main']['pressure']
+    windSpeed = conteudo['wind']['speed']
+    data = datetime.datetime.now()
+    dadosClimaticos = DadosClimaticos(temp, humidity, windSpeed, pressure, data, lat, lon)
+    session.add(dadosClimaticos)
+    session.commit()
+    return dadosClimaticos
+
+def getDadosClimaticos():
+    dadosClimaticos = []
+    for dado in session.query(DadosClimaticos).all():
+        dadosClimaticos.append(dado)
+        print(dado)
+    return dadosClimaticos
