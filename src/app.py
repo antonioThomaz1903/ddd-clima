103b2fd809a9d9c27550298e44c6c841c50fc5cd
antonioThomaz1903 <antoniotor987@gmail.com>
2024-06-23 22:31:09 -0400

    refactor: retirando metodos jwt que nao estavam sendo utilizados

--- src/app.py
+++ src/app.py
@@ -14,17 +14,6 @@ BaseUsuario.metadata.create_all(engine)
 Session_app = sessionmaker(bind=engine)
 session_app = Session_app()
 
-def generate_jwt(payload):
-    token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-    return token
-
-def verify_jwt(token):
-    try:
-        header, payload, signature = jose.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        return payload
-    except jose.exceptions.JWTError as e:
-        return None
-
 
 if __name__ == "__main__":
     from src.Services.Api import api_bp
