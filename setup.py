55c199da1ca496f48e9174418dd337b327c9eddb
antonioThomaz1903 <antoniotor987@gmail.com>
2024-06-23 22:32:11 -0400

    chore: adicionando setup.py para buildar projeto

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    version='0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'flask',
+        'sqlalchemy',
+        'flask_sqlalchemy',
+        'dynaconf',
+        'jose',
+        'flask-restx',
+    ],
+    entry_points={
+        'console_scripts': [
+            'my_project=app:main',
+        ],
+    },
+)
